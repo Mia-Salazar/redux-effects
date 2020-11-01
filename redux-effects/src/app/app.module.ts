36a1730683d88d2cc4a85e9e0090f221f4fc1a4f
Mia Salazar <maria.sgr@gmail.com>
2020-11-01 13:56:49 +0100

    Add routing

--- redux-effects/src/app/app.module.ts
+++ redux-effects/src/app/app.module.ts
@@ -1,24 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
-import { ListComponent } from './users/list/list.component';
-import { UserComponent } from './users/user/user.component';
 import { UsersModule } from './users/users.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent,
-    UserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ComponentsModule,
-    UsersModule
+    UsersModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
